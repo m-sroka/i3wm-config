049b6821dab099e95c31073191564b2b6aa13eef
Maciej Sroka <maciej.sroka2@gmail.com>
2019-02-10 00:00:47 +0000

    Tuning status bar: custom script, volume, brightness, cpu, battery

---
+++ i3statuscustom.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+i3status -c $HOME/.config/i3/i3status.conf | while :
+do
+        read line
+        volume=`$HOME/.config/i3/get_volume.sh`
+        brightness=`$HOME/.config/i3/get_brightness.sh`
+        echo "BR: $brightness | VOL: $volume | $line" || exit 1
+done
