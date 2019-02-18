f1931bac86a3cb81ba44f111444718e5076ce1e5
Maciej Sroka <maciej.sroka2@gmail.com>
2019-02-18 20:04:42 +0000

    Enable natural scrolling when multiple usb receivers connected

--- set_natural_scrolling.sh
+++ set_natural_scrolling.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 touchpad_id=`exec xinput list | grep -i touchpad | cut -f 2 | cut -f 2 -d'='`
-exec xinput --set-prop $touchpad_id "libinput Natural Scrolling Enabled" 1
+xinput --set-prop $touchpad_id "libinput Natural Scrolling Enabled" 1
+
+usb_receivers_ids=`exec xinput list | grep -i "usb receiver" | cut -f 2 | cut -f 2 -d'='`
+for id in $usb_receivers_ids
+do
+  exec xinput --set-prop $id "libinput Natural Scrolling Enabled" 1
+done
 
