362cf17184a975371cdb30935a3a146b31e19e81
Maciej Sroka <maciej.sroka2@gmail.com>
2019-12-07 01:10:44 +0000

    Volume change and progressbar per active sink

--- get_volume.sh
+++ get_volume.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-echo `pactl list sinks | grep "^[[:space:]]*\ *Volume:" | awk '{print $5}'`
+# echo `pactl list sinks | grep "^[[:space:]]*\ *Volume:" | awk '{print $5}'`
+
+echo $(pactl list sinks | grep `pacmd stat | awk -F": " '/^Default sink name: /{print $2}'` -A 20 | grep -i '^\svolume'  | awk '{print $5}')
--- volume_change.sh
+++ volume_change.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 stereo_sink=`pactl list short | grep stereo | head -n 1 | cut -f 1`
+b_sink=`pactl list short | grep '08_A3_0B_08_7F_92' | head -n 1 | cut -f 1`
+bluez_sink=`pactl list short | grep bluez_sink | head -n 1 | cut -f 1`
 
-pactl set-sink-volume $stereo_sink $1
+#pactl set-sink-volume $stereo_sink $1
+#pactl set-sink-volume $b_sink $1
+#pactl set-sink-volume $bluez_sink $1
+pactl set-sink-volume @DEFAULT_SINK@ $1
